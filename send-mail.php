857ef6d0d0888283aea4106823b3c29b48b9952b
MUTHU2717 <166125717+MUTHU2717@users.noreply.github.com>
2025-12-01 21:19:11 +0530

    Update send-mail.php

--- send-mail.php
+++ send-mail.php
@@ -1,38 +1,47 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $name       = htmlspecialchars($_POST['name']);
-    $email      = htmlspecialchars($_POST['email']);
-    $mobile     = htmlspecialchars($_POST['mobile']);
-    $org        = htmlspecialchars($_POST['organization']);
-    $message    = htmlspecialchars($_POST['message']);
+// Owner email
+$owner_email = "muthukumar418707@gmail.com";
 
-    $to = "muthukumar418707@gmail.com";  // <-- Your email ID
-    $subject = "New Consultation Request from Your Website";
+// Collect form values
+$name   = $_POST['name'];
+$email  = $_POST['email'];
+$mobile = $_POST['mobile'];
+$org    = $_POST['organization'];
+$msg    = $_POST['message'];
 
-    $body = "
-    You have received a new message from your website contact form.
+// Email to YOU (the business owner)
+$subject = "New Consultation Request From Website";
 
-    ----------------------------------------------------
-    Name: $name
-    Email: $email
-    Mobile No: $mobile
-    Organization: $org
-    Message:
-    $message
-    ----------------------------------------------------
-    ";
+$message_to_owner  = "You have a new consultation request:\n\n";
+$message_to_owner .= "Name: $name\n";
+$message_to_owner .= "Email: $email\n";
+$message_to_owner .= "Mobile: $mobile\n";
+$message_to_owner .= "Organization: $org\n\n";
+$message_to_owner .= "Message:\n$msg\n";
 
-    $headers = "From: Website Contact Form <no-reply@yourdomain.com>\r\n";
-    $headers .= "Reply-To: $email\r\n";
+$headers_owner = "From: $email";
 
-    if (mail($to, $subject, $body, $headers)) {
-        echo "<script>alert('Your message has been sent successfully!'); window.location.href='contact.html';</script>";
-    } else {
-        echo "<script>alert('Failed to send your message. Please try again later.'); window.location.href='contact.html';</script>";
-    }
+// Send email to owner
+mail($owner_email, $subject, $message_to_owner, $headers_owner);
 
-} else {
-    echo "Invalid request!";
-}
+
+// Auto-reply to the USER
+$reply_subject = "Thanks for contacting us!";
+$reply_message = "Hello $name,\n\n";
+$reply_message .= "Thank you for reaching out. We will get back to you soon.\n";
+$reply_message .= "In the meantime, visit our website for more information.\n\n";
+$reply_message .= "Regards,\nAnurag Consultancy";
+
+$headers_reply = "From: $owner_email";
+
+// Send auto-reply
+mail($email, $reply_subject, $reply_message, $headers_reply);
+
+
+// Redirect user to a thank-you page
+header("Location: thank-you.html");
+exit();
+
 ?>
+
